ea9e8d1eb84646377f4e977f4250abb3bc33d8ab
Espen Westgaard <espenwest@gmail.com>
2022-08-01 13:48:16 +0200

    Black reformatted communicator

--- src/embodyble/communicator.py
+++ src/embodyble/communicator.py
@@ -1,6 +1,5 @@
 """Communicator module to communicate with an EmBody device over BLE (Bluetooth).
 
-Allows for both sending messages synchronously and asynchronously, receiving response messages
-and subscribing for incoming messages from the device.
+Allows for both sending messages synchronously and asynchronously,
+receiving response messages and subscribing for incoming messages from the device.
 """
-
