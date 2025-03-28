539cf8af7c87971f1371643ff8f0964df52ac73e
Espen Westgaard <espenwest@gmail.com>
2025-03-28 07:06:29 +0100

    Use python 3.12 for nox

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ except ImportError:
 
 
 package = "embodyble"
-python_versions = ["3.11"]
+python_versions = ["3.12"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
