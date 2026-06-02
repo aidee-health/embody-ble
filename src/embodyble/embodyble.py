4596b459b7d63f8762c98a976be59b7c7c2c09d7
Kyrre Aalerud <kyrreaa@gmail.com>
2026-06-02 15:19:32 +0200

    Enlarge MAX_SAVED_DATA_SIZE to allow some backlog. The data chunks for filetransfer is 4096 bytes and with header+crc it makes 4106 bytes. Thus, 4096 will never allow a full packet to be received and there is no room for backlog during processing.

--- src/embodyble/embodyble.py
+++ src/embodyble/embodyble.py
@@ -36,7 +36,7 @@ UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-MAX_SAVED_DATA_SIZE = 4096  # Maximum size for incomplete message buffer
+MAX_SAVED_DATA_SIZE = 40960  # Maximum size for incomplete message buffer
 
 
 class EmbodyBle(embodyserial.EmbodySender):
